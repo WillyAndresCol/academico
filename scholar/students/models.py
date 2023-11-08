fcaf27d75817b9027f0c22ff67e3da5409e9a799
Laura <lsgomez.5747@unicesmag.edu.co>
2023-11-07 19:58:19 -0500

    nuevos modelos

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -1,7 +1,22 @@
 from django.db import models
+from django.utils import timezone
+import datetime
+
 
 class User(models.Model):
-    email = models.CharField(max_length = 100)
-    password = models.CharField(max_length = 500)
-    dni = models.CharField(max_length = 10, default="")
-    age = models.IntegerField(default = 0)
+    email = models.CharField(max_length = 100, null = False)
+    password = models.CharField(max_length = 500, null = False)
+    status = models.BooleanField(default=True, null = True)
+    create_at = models.DateTimeField(default=datetime.datetime.now, null = False)
+    update_at = models.DateTimeField(default=datetime.datetime.now, null = False)
+    deleted_at = models.DateTimeField(null = True)
+
+class Identification_types(models.Model):
+    name = models.CharField(max_length = 50, null = False)
+    abrev = models.CharField(max_length = 10, null = False)
+    descrip = models.CharField(max_length = 100, null = False)
+    create_at = models.DateTimeField(default=datetime.datetime.now, null = False)
+    update_at = models.DateTimeField(default=datetime.datetime.now, null = False)
+    deleted_at = models.DateTimeField(null = True)
+
+
